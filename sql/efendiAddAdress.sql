db79c4c9e5b93482a28746db55847e4d359dcde7
DemirDorukDilekAsus <demirdorukd@gmail.com>
2025-11-28 18:44:37 +0300

    "%s" hata güncerleme. not olasu db update gerekeblir

--- sql/efendiAddAdress.sql
+++ sql/efendiAddAdress.sql
@@ -1,1 +1,1 @@
-INSERT INTO Adres(efendiID,adresName,il,ilce,mah,cd,binano,daireno,X,Y) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")
+INSERT INTO Adres(efendiID,adresName,il,ilce,mah,cd,binano,daireno,X,Y) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
